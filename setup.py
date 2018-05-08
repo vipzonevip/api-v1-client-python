fbe998355871eb6cc03af8c0677ed514141e551b
ray <ray@blockchain.com>
2018-05-08 10:54:35 +0100

    change dependency of enum to enum34 to work with python3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='blockchain',
       license='MIT',
       packages=['blockchain', 'blockchain/v2'],
       keywords='blockchain.info api blockchain',
-      install_requires=['enum', 'future'],
+      install_requires=['enum34', 'future'],
       classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",

a107a5b833e61859cab872e3bc6b6d49fb2c478e
Fabian Affolter <fabian@affolter-engineering.ch>
2018-05-25 17:09:45 +0200

    Prepare for 1.4.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='blockchain',
-      version='1.4.3',
+      version='1.4.4',
       description='Blockchain API library (v1)',
       author='Blockchain.info',
       author_email='support@blockchain.zendesk.com',
